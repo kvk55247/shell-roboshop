3fcc61c170a8212d61d4453031139c82fcb86007
kvk55247 <kurapothulavamsi@gmail.com>
2025-07-27 22:51:06 +0530

    spaecial vars

--- roboshop.sh
+++ roboshop.sh
@@ -11,9 +11,11 @@ do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0df304cc4c6711e85 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].privateIpAddress" --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations
+        [0].Instances[0].privateIpAddress" --output text)
     else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations 
+        [0].Instances[0].PublicIpAddress" --output text)
     fi
     echo "$instance IP address: $IP"
 done
