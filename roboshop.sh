18e0ab9b930d83ab3b88bf07b0ce3c8d11b84e32
kvk55247 <kurapothulavamsi@gmail.com>
2025-07-28 16:41:46 +0530

    hello world started

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,9 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "patme
 ZONE_ID="Z02592383JVQTDY6U9ADB"
 DOMAIN_NAME="daws84s.info"
 
+
 for instance in ${INSTANCES[@]}
+do
 
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0df304cc4c6711e85 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
@@ -16,4 +18,4 @@ for instance in ${INSTANCES[@]}
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations [0].Instances[0].PublicIpAddress" --output text)
     fi
     echo "$instance IP address: $IP"
-do
+done
