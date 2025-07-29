a61dc8de294e957b146dae2eb96c611e17c5fa36
kvk55247 <kurapothulavamsi@gmail.com>
2025-07-29 12:10:38 +0530

    special vars

--- route-53.sh
+++ route-53.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
-echo "hello world"
+
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0df304cc4c6711e85"
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z02592383JVQTDY6U9ADB"
+DOMAIN_NAME="daws-84s.info"
+
+
+do
+aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "Creating or updating a record set for cognito endpoint"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$instance'.'$DOMAIN_NAME"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "$IP"
+        }]
+      }
+    }]
+  }'
+done
