d32742daff56820dc44e0ff7d690fad2fcfb64bc
kvk55247 <kurapothulavamsi@gmail.com>
2025-07-30 19:09:03 +0530

    special vars

--- ip.sh
+++ ip.sh
@@ -1,39 +1,28 @@
 #!/bin/bash
-#!/bin/bash
 
-# Input variables
-HOSTED_ZONE_ID="Z02592383JVQTDY6U9ADB"  # Replace with your actual Hosted Zone ID
+
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0df304cc4c6711e85"
+INSTANCES=("mongodb" "catalogue" "frontend")
+ZONE_ID="Z02592383JVQTDY6U9ADB"
 DOMAIN_NAME="daws84s.info"
-RECORD_TYPE="A"
-RECORD_TTL=1
-RECORD_VALUE="$IP"  # Replace with your IP or record value
-
-# Create the JSON file for the change batch
-cat > change-batch.json << EOF
-{
-  "Comment": "UPSERT record via shell script",
-  "Changes": [
-    {
-      "Action": "UPSERT",
-      "ResourceRecordSet": {
-        "Name": "'$instance'.'$DOMAIN_NAME",
-        "Type": "A",
-        "TTL": 1,
-        "ResourceRecords": [
-          {
-            "Value": "$IP"
-          }
-        ]
-      }
-    }
-  ]
-}
-EOF
-
-# Call AWS CLI to apply the change
+   
 aws route53 change-resource-record-sets \
-    --hosted-zone-id "$HOSTED_ZONE_ID" \
-    --change-batch file://change-batch.json
-
-# Clean up
-rm change-batch.json
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "Creating or updating a record set for cognito endpoint"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$instance'.'$DOMAIN_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }'
+  
