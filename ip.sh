cda73776d92b58eccf4bbfca415c8cbf86765672
kvk55247 <kurapothulavamsi@gmail.com>
2025-07-31 11:22:14 +0530

    special vars

--- ip.sh
+++ ip.sh
@@ -26,3 +26,21 @@ aws route53 change-resource-record-sets \
     }]
   }'
   
+  
+  aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "Creating or updating a record set for cognito endpoint"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$instance'.'$DOMAIN_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }'
