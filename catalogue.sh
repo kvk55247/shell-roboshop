e71a6ef8a033fd0833d69eb1713c8ac6fb6be722
kvk55247 <kurapothulavamsi@gmail.com>
2025-07-31 19:46:58 +0530

    hello world started

---
+++ catalogue.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m]"
+LOGS_FOLDER="/var/logs/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "script started executing at: $(date)" tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: please run this script with root access $N"  tee -a $LOG_FILE
+    exit 1
+else
+    echo "you are running with root access"  tee -a $LOG_FILE
+fi
+
+VALIDATE(){
+ if [ $? -eq 0 ]
+then
+    echo "$2 is .... success"  tee -a $LOG_FILE
+else
+    echo " $2 is ....FAILURE"  tee -a $LOG_FILE
+    exit 1
+fi
+}
+dnf module disable nodejs -y
+VALIDATE $? "disbaling default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling nodejs:20"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "installing nodejs"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+VALIDATE $? "Creating roboshop system user"
+
+mkdir /app &>>$LOG_FILE
+VALIDATE $? "creating directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+VALIDATE $? "downloading catalogue"
+
+cd /app 
+unzip /tmp/catalogue.zip
+VALIDATE $? "unzipping catalogue"
+
+npm install &>>$LOG_FILE
+VALIDATE $? "installing dependencies"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
+VALIDATE $? "copying catalogue.service"
+
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable catalogue &>>$LOG_FILE
+systemctl start catalogue
+VALIDATE $? "starting catalogue"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "installing mongoDB client"
+
+mongosh --host mongodb.daws84s.info </app/db/master-data.js &>>$LOG_FILE
