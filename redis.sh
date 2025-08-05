829186c31dcf8ddcd4f3771a1ccfeaf07e3cb981
kvk55247 <kurapothulavamsi@gmail.com>
2025-08-05 17:55:31 +0530

    hello world started

---
+++ redis.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m]"
+LOGS_FOLDER="/var/logs/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "script started executing at: $(date)" tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: please run this script with root access $N"  tee -a $LOG_FILE
+    exit 1
+else
+    echo "you are running with root access"  tee -a $LOG_FILE
+fi
+
+VALIDATE(){
+ if [ $? -eq 0 ]
+then
+    echo "$2 is .... success"  tee -a $LOG_FILE
+else
+    echo " $2 is ....FAILURE"  tee -a $LOG_FILE
+    exit 1
+fi
+}
+dnf module disable redis -y
+VALIDATE $? "disabling default redis"
+
+
+dnf module enable redis:7 -y
+VALIDATE $? "enabling redis:7"
+
+dnf install redis -y 
+VALIDATE $? "installing redis"
+
+ sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf 
+VALIDATE $? "Editing redis conf file for rempote connections"
+
+systemctl enable redis 
+VALIDATE $? "enabling redis"
+
+systemctl start redis 
+VALIDATE $? "starting redis"
