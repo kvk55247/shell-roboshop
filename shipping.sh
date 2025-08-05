489219f6ebd308a3fa58f7f192fc39fc661a730e
kvk55247 <kurapothulavamsi@gmail.com>
2025-08-05 20:18:50 +0530

    hello world started

---
+++ shipping.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m]"
+LOGS_FOLDER="/var/logs/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "script started executing at: $(date)" tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: please run this script with root access $N"  tee -a $LOG_FILE
+    exit 1
+else
+    echo "you are running with root access"  tee -a $LOG_FILE
+fi
+
+VALIDATE(){
+ if [ $? -eq 0 ]
+then
+    echo "$2 is .... success"  tee -a $LOG_FILE
+else
+    echo " $2 is ....FAILURE"  tee -a $LOG_FILE
+    exit 1
+fi
+}
+dnf install maven -y &>>$LOG_FILE
+VALIDATE $? "installing maven"
+
+
+id roboshop
+if [ $? -ne 0 ]
+then
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+VALIDATE $? "Creating roboshop system user"
+else
+echo -e "systemuser roboshop already created..... $Y SKIPPING $N"
+fi
+
+
+mkdir -p /app &>>$LOG_FILE
+VALIDATE $? "creating directory"
+
+curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
+VALIDATE $? "downloading catalogue"
+
+rm -rf /app/* &>>$LOG_FILE
+cd /app 
+unzip /tmp/shipping.zip &>>$LOG_FILE
+VALIDATE $? "unzipping shipping"
+
+
+mvn clean package &>>$LOG_FILE
+VALIDATE $? "packaging the maven application"
+
+mv target/shipping-1.0.jar shipping.jar 
+VALIDATE $? "moving and renaming jar file"
+
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service &>>$LOG_FILE
+VALIDATE $? "copying shipping.service"
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "daemon reload"
+
+systemctl enable shipping &>>$LOG_FILE
+systemctl start shipping
+VALIDATE $? "starting shipping"
+
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "installing mysql"
+
+mysql -h mysql.daws84s.info -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
+mysql -h mysql.daws84s.info -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE
+mysql -h mysql.daws84s.info -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+VALIDATE $? "loading data into mysql"
+
+systemctl restart shipping &>>$LOG_FILE
+VALIDATE $? "restarting shipping"
+
+
+END_TIME=$(date +%s) &>>$LOG_FILE
+TOTAL_TIME=$(( $END_TIME - $START_TIME)) &>>$LOG_FILE
+
+echo -e "script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+
