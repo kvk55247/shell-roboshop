86cb1aa0aa88632bfce51f9ae0279565f21f42f7
kvk55247 <kurapothulavamsi@gmail.com>
2025-08-05 14:56:50 +0530

    hello world started

--- catalogue.sh
+++ catalogue.sh
@@ -41,7 +41,7 @@ dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "installing nodejs"
 
 id roboshop
-if [ $? -ne o ]
+if [ $? -ne 0 ]
 then
 useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
 VALIDATE $? "Creating roboshop system user"
